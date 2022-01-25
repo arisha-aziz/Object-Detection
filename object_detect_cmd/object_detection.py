557c50c7b95363187cd9353d3b4b54b0fae1453d
Arisha Aziz <68965699+arisha-aziz@users.noreply.github.com>
2022-01-25 11:57:11 +0500

    colab compatible + minor other changes

--- object_detect_cmd/object_detection.py
+++ object_detect_cmd/object_detection.py
@@ -65,7 +65,7 @@ class MobileNetSSD:
                     box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                     # setting the four core coordinates to draw the rectangle
                     (startX, startY, endX, endY) = box.astype("int")
-                    label = f"{CLASSES[idx]} : {float(round(confidence, 3))}"
+                    label = f"{CLASSES[idx]} : {round(confidence, 2))}"
                     # increment every time it is detected
                     detected += 1
                     cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
